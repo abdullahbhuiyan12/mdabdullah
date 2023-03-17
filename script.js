768d3948f33a3fccc63bd73dd50b8cef55f6dc06
Md Abdullah bhuiyan <127809580+abdullahbhuiyan12@users.noreply.github.com>
2023-03-16 21:55:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleIcon = document.querySelector('.toggle-icon');
+
+toggleIcon.addEventListener('click', () => {
+    toggleIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+});
